0db642715d0b277d71bf119a5ea9dfdc8e8a2cf0
Catherine <catherine@mac-1.home>
2025-07-03 12:36:39 +0200

    première version de l'exercice

---
+++ calculate-php-1/tests/utils/BinaryCalculateTest.php
@@ -0,0 +1,56 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Catherine\CalculatePhp1\utils\BinaryCalculate;
+
+class BinaryCalculateTest extends TestCase
+{
+    private $binaryCalculate;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->binaryCalculate = new BinaryCalculate();
+    }
+
+    public function testAndBinaryCalcul()
+    {
+        // GIVEN
+        $a = 4;
+        $b = 1;
+        $expected = 0; // 100 & 001 = 000
+
+        // WHEN
+        $result = $this->binaryCalculate->andBinaryCalcul($a, $b);
+
+        // THEN
+        $this->assertEquals($expected, $result);
+    }
+        public function testOrBinaryCalcul()
+    {
+        // GIVEN
+        $a = 4;
+        $b = 1;
+        $expected = 5; // 100 | 001 = 101
+
+        // WHEN
+        $result = $this->binaryCalculate->orBinaryCalcul($a, $b);
+
+        // THEN
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testXorBinaryCalcul()
+    {
+        // GIVEN
+        $a = 4;
+        $b = 1;
+        $expected = 5; // 100 ^ 001 = 101
+
+        // WHEN
+        $result = $this->binaryCalculate->xorBinaryCalcul($a, $b);
+
+        // THEN
+        $this->assertEquals($expected, $result);
+    }
+}
