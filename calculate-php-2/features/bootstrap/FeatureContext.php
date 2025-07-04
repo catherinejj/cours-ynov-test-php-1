d2c5d22cbb52e7628d311f04955dc7c24021ea8e
catherinejj <catherine.jules@ynov.com>
2025-07-04 16:58:27 +0200

    mise a jour de la feature et utilisation de Behat pour une première version du context

--- calculate-php-2/features/bootstrap/FeatureContext.php
+++ calculate-php-2/features/bootstrap/FeatureContext.php
@@ -1,8 +1,13 @@
 <?php
 
+use Symfony\Component\DomCrawler\Crawler;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Step\Given;
+use Behat\Step\When;
+use Behat\Step\Then;
 
 /**
  * Defines application features from the specific context.
@@ -18,5 +23,36 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+    //--- FeatureContext has missing steps. Define them with these snippets:
+
+    #[Given('je suis sur :arg1')]
+    public function jeSuisSur($arg1): void
+    {
+        throw new PendingException();
+    }
+
+    #[Given('je remplis :arg1 avec :arg2')]
+    public function jeRemplisAvec($arg1, $arg2): void
+    {
+        throw new PendingException();
+    }
+
+    #[Given('je sélectionne :arg1 dans :arg2')]
+    public function jeSélectionneDans($arg1, $arg2): void
+    {
+        throw new PendingException();
+    }
+
+    #[When('je clique sur :arg1')]
+    public function jeCliqueSur($arg1): void
+    {
+        throw new PendingException();
+    }
+
+    #[Then('je devrais voir :arg1')]
+    public function jeDevraisVoir($arg1): void
+    {
+        throw new PendingException();
     }
 }
--- calculate-php-2/features/calculatrice.feature
+++ calculate-php-2/features/calculatrice.feature
@@ -2,8 +2,9 @@ Feature: Calculatrice Binaire
 
   Scenario: L'utilisateur calcule 4 AND 1
     Given je suis sur "hhttp://localhost:8000"
-    And je saisis "4" dans le champ "a"
-    And je saisis "1" dans le champ "b"
+    And je remplis "a" avec "4"
+    And je remplis "b" avec "1"
     And je sélectionne "and" dans "op"
-    When je soumets le formulaire
-    Then je devrais voir "Résultat de and entre 4 et 1 : 0"
+    When je clique sur "Go !"
+    Then je devrais voir "Résultat de and entre 4 et 1 :"
+    And je devrais voir "Résultat en binaire :"
